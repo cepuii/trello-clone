f0590890dde20629de456a2f9da35149574d39bd
cepuii <54589413+cepuii@users.noreply.github.com>
2024-02-24 20:07:43 +0200

    Add image info to schema

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -13,6 +13,7 @@ import { FormSubmit } from "./form-submit";
 import { useAction } from "@/hooks/useAction";
 import { createBoard } from "@/actions/create-board";
 import { toast } from "sonner";
+import { FormPicker } from "./form-picker";
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -40,8 +41,9 @@ export const FormPopover = ({
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get("title") as string;
+    const image = formData.get("image") as string;
 
-    execute({ title });
+    execute({ title, image });
   };
 
   return (
@@ -65,7 +67,8 @@ export const FormPopover = ({
           </Button>
         </PopoverClose>
         <form action={onSubmit} className="space-y-4">
-          <div>
+          <div className="space-y-4">
+            <FormPicker id="image" errors={fieldErrors} />
             <FormInput
               id="title"
               type="text"
