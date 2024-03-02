17f0ed88eabad66517e54956c8a38a369af4291b
cepuii <54589413+cepuii@users.noreply.github.com>
2024-03-02 12:43:53 +0200

    Add Action to cerate card

---
+++ actions/create-card/index.ts
@@ -0,0 +1,66 @@
+"use server";
+
+import { auth } from "@clerk/nextjs";
+import { InputType, ReturnType } from "./types";
+import { db } from "@/lib/db";
+import { createSafeAction } from "@/lib/create-safe-action";
+import { CreateCard } from "./schema";
+import { revalidatePath } from "next/cache";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return { error: "Unathorized" };
+  }
+
+  const { title, boardId, listId } = data;
+
+  let card;
+
+  try {
+    const list = await db.list.findUnique({
+      where: {
+        id: listId,
+        board: {
+          orgId,
+        },
+      },
+    });
+
+    if (!list) {
+      return {
+        error: "List not found",
+      };
+    }
+
+    const lastCard = await db.card.findFirst({
+      where: {
+        listId,
+      },
+      orderBy: {
+        order: "desc",
+      },
+      select: {
+        order: true,
+      },
+    });
+
+    const newOrder = lastCard ? lastCard.order + 1 : 1;
+
+    card = await db.card.create({
+      data: {
+        title,
+        order: newOrder,
+        listId,
+      },
+    });
+  } catch (error) {
+    return { error: "Failed to create card" };
+  }
+
+  revalidatePath(`/board/${boardId}`);
+  return { data: card };
+};
+
+export const createCard = createSafeAction(CreateCard, handler);
